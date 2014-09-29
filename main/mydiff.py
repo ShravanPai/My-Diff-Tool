19560505a0e39b37f3a1cfe7e60b5cc682b37895
Shravan Pai <glxpai@gmail.com>
2014-09-28 17:21:05 -0700

    Algorithm implemented

--- main/mydiff.py
+++ main/mydiff.py
@@ -3,12 +3,19 @@ Created on Sep 18, 2014
 
 @author: shravan
 '''
+
+import sys
 from sys import argv
 from timeit import itertools
 from numpy import *
+from termcolor import colored
+ 
 
 def entry():
     # Get source and target file contents
+    if len(argv) != 3:
+        sys.exit('Error!! \n Format :  mydiff.py <Source File> <Target File>')
+        
     source_contents = get_contents(argv[1])
     target_contents = get_contents(argv[2])
     
@@ -29,8 +36,43 @@ def get_diff(source_contents, target_contents):
     for source_line, target_line in itertools.izip(source, target):
         source_length = len(source_line)
         target_length = len(target_line)
-        calc_matrix = zeros((source_length, target_length))
-
+        calc_matrix = zeros((source_length + 1, target_length + 1))
+        sequence_length = calculate_sequence_length(calc_matrix,
+                                                    source_length + 1, 
+                                                    target_length + 1,
+                                                    source_line, 
+                                                    target_line)
+        if int(sequence_length) != source_length:
+            print 'Line Number : ' + str(line_number)
+            print colored(' - ' + source_line, 'red')
+            print colored(' + ' + target_line, 'green')
+            print '----------------------------------------------------------'
+        else:
+            print 'Line Number : ' + str(line_number) + ' : No Change'
+            print '----------------------------------------------------------'
+            
+        line_number += 1
+        
+# returns the sequence length, given two lines
+def calculate_sequence_length(calc_matrix, src_len, trg_len, src_line,
+                               trg_line):
+    
+    # Convert the strings to lowercase
+    src_line = src_line.lower()
+    trg_line = trg_line.lower()
+    
+    # Apply LCS algorithm
+    for i in range (1, src_len):
+        for j in range (1, trg_len):
+            if src_line[i-1] == trg_line[j-1]:
+                calc_matrix[i][j] = calc_matrix[i-1][j-1] + 1
+            else:
+                calc_matrix[i][j] = max(calc_matrix[i-1][j],
+                                        calc_matrix[i][j-1])
+                
+    # return the longest match
+    return calc_matrix[i][j]
+                
 
 if __name__ == '__main__':
     entry()
